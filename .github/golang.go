ec72727945bc82afdf718bc30860a2f958e6ac77
Pedro Virmond Prates Correia <pedro.vc@sempreceub.com>
2023-05-09 09:36:55 -0300

    Algoritmos - Ponteiros

---
+++ .github/golang.go
@@ -0,0 +1,10 @@
+// Escreva uma função que receba um ponteiro para um booleano e altere o valor desse
+// booleano para o seu inverso.
+package main
+
+func invert(ptr *bool) {
+
+}
+func main() {
+
+}
