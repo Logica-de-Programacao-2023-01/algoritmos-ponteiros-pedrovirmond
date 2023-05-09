4c1c5b220a8a78d05114f12fed61a4ad85381df8
Pedro Virmond Prates Correia <pedro.vc@sempreceub.com>
2023-05-09 10:12:33 -0300

    Algoritmos - Ponteiros

--- .github/golang.go
+++ .github/golang.go
@@ -1,10 +1,23 @@
-// Escreva uma função que receba um ponteiro para um booleano e altere o valor desse
-// booleano para o seu inverso.
+// Escreva uma função que receba um ponteiro para uma struct que contém informações
+// de um produto (nome, preço e quantidade em estoque). A função deve atualizar o preço
+// desse produto para um novo valor fornecido como argumento.
 package main
 
-func invert(ptr *bool) {
+import "fmt"
 
+type Produto struct {
+	Nome       string
+	Preço      float64
+	Quantidade int
 }
-func main() {
 
+func updatePreço(p *Produto, newPreço float64) {
+	p.Preço = newPreço
+}
+
+func main() {
+	p := Produto{"Calça jeans", 69.99, 43}
+	fmt.Println("Preço antes da atualização: ", p.Preço)
+	updatePreço(&p, 52.99)
+	fmt.Println("Preço depois da atualização", p.Preço)
 }
